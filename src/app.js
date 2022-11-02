0f342aa194686b0c4302929456945fd4c77663ee
Andres Copeticona <andrescopeticona@outlook.es>
2022-11-02 18:26:05 -0400

    remove cors

--- src/app.js
+++ src/app.js
@@ -4,17 +4,17 @@ import cors from 'cors';
 
 const app = express();
 
-let corsOptions = {
-	origin: function (origin, cb) {
-		if (origin.includes('grupoether.xyz')) {
-			cb(null, true);
-		} else {
-			cb(new Error('Not allowed by CORS'));
-		}
-	},
-};
+// let corsOptions = {
+// 	origin: function (origin, cb) {
+// 		if (origin.includes('grupoether.xyz')) {
+// 			cb(null, true);
+// 		} else {
+// 			cb(new Error('Not allowed by CORS'));
+// 		}
+// 	},
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.set('PORT', process.env.PORT || 3000);
 
