29821134fe260040fb328191a9b2c9607ad1841a
Andres Copeticona <andrescopeticona@outlook.es>
2022-11-02 18:19:36 -0400

    test cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 import express from 'express';
 import colorsRoutes from './routes/colors.routes.js';
+import cors from 'cors';
 
 const app = express();
+
+let corsOptions = {
+	origin: function (origin, cb) {
+		if (/\.grupoether\.xyz$/.test(origin)) {
+			cb(null, true);
+		} else {
+			cb(new Error('Not allowed by CORS'));
+		}
+	},
+};
+
+app.use(cors(corsOptions));
 
 app.set('PORT', process.env.PORT || 3000);
 
